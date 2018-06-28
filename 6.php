aec34be50a135cafe9576a66c9560677013a21d1
nihirsharma04 <nihirsharma04@gmail.com>
2018-06-28 12:25:30 +0530

    Assignment 1

---
+++ 6.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+<style type="text/css">
+.header{
+	width:100%;
+	height:200px;
+	background-color:#FFFFFF;
+	float:left;
+	text-align:center;
+	font-size:50px;
+	color:#000000;
+	border-color:#000000;
+	}
+
+.menu{
+	width:100%;
+	height:50px;
+	background-color:#009900;
+	float:left;
+	text-align:center;
+	font-size:50px;
+	color:#CC0000;
+	border-color:#000000;
+	
+
+
+	}
+	.image{
+width:100%;
+	height:250px;
+	background-color:#CCCCCC;
+	float:left;
+	font-size:30px;
+	color:#CC0000;
+	border-color:#000000;
+	}
+	.leftcontent{
+	width:49.5%;
+	height:350px;
+	background-color:#FFFFFF;
+	float:left;
+	text-align:center;
+	font-size:30px;
+	color:#CC0000;
+	border-color:#000000;
+	}
+	.rightcontent{
+	width:49.5%;
+	height:350px;
+	background-color:#FFFFFF;
+	float:left;
+	text-align:center;
+	font-size:30px;
+	color:#CC0000;
+	border-color:#000000;
+	}
+	
+	
+	.footer
+	{
+	width:100%;
+	height:50px;
+	border:dotted;
+	background-color:#000000;
+	float:left;
+	text-align:center;
+	font-size:30px;
+	color:#FFFFFF;
+	border-color:#000000;
+	}
+	
+	</style>
+</head>
+<body>
+<div class="header">
+<img src="logo.png" />
+VIDYA KNOWLEDGE PARK
+</div>
+<div class="menu">
+
+</div>
+<div class="image">
+<img src="vkp2.jpg" width=80% />
+</div>	
+<div class="leftcontent">
+LEFTCONTENT
+</div>
+
+<div class="rightcontent">
+CONTACT FORM 
+<form method="get">
+<div class="frontform"><input type="text" name="uname" placeholder="Your Name" required="required"  /></div>
+<div class="frontform"><input type="email" name="uemail" placeholder="Your Email" required="required"  /></div>
+<div class="frontform"><input type="text" name="contact" placeholder="Your Contact" maxlength="10" required="required"  /></div>
+<div class="frontform">Male: <input type="radio" name="gender" value="male" /> |
+Fe-Male: <input type="radio" name="gender" value="female" /></div>
+
+<div class="frontform">
+<select name="city" required="required">
+<option value="">Select City </option><option value="Delhi">Delhi</option><option value="Meerut">Meerut</option>
+</select></div>
+<input type="submit" name="submit"  />
+</div>
+<div class="footer">
+vidya knowledge park ,pachli khurdh ,bagpat road meerut
+</div>
+</body>
+
+</body>
+</html>
+
+
+<body>
+</body>
+</html>
